253e84a314755cfd528d34fec2fc037b11bf5e87
DavidOleas26 <182365598+David-kruger@users.noreply.github.com>
2025-01-04 20:08:49 -0500

    reestructuracion de las carpetas

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-emerald-600'>Tailwind</h1>
+      <p className='text-red-800'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta totam illo cumque quidem accusantium quas? Eligendi, nobis laboriosam est error voluptatem quis reiciendis corporis accusamus, itaque impedit, sit explicabo natus?</p>
+    </>
+  )
+}
+
+export default App
