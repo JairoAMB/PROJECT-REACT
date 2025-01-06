b8a674846ccc2cc1ddcfd5c09237a0af1fbaad0e
Jairo <jmasabanda@krugerschool.edu.ec>
2025-01-06 18:27:47 -0500

    page login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -16,36 +16,36 @@ const Login = () => {
     return (
         <div className="login-container">
             <h1>Iniciar sesión</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="input-group">
-                    <label htmlFor="email">Correo electrónico:</label>
-                    <input
-                        type="email"
-                        id="email"
-                        placeholder="Introduce tu correo"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="input-group">
-                    <label htmlFor="password">Contraseña:</label>
-                    <input
-                        type="password"
-                        id="password"
-                        placeholder="Introduce tu contraseña"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                {errorMessage && <p className="error">{errorMessage}</p>}
-                <button type="submit" className="login-button">Iniciar sesión</button>
-            </form>
-            <div className="register-link">
-                <p>No tienes cuenta? <a href="/register">Regístrate aquí</a></p>
+        <form onSubmit={handleSubmit}>
+            <div className="input-group">
+                <label htmlFor="email">Correo electrónico:</label>
+                <input
+                    type="email"
+                    id="email"
+                    placeholder="Introduce tu correo"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
             </div>
+            <div className="input-group">
+                <label htmlFor="password">Contraseña:</label>
+                <input
+                    type="password"
+                    id="password"
+                    placeholder="Introduce tu contraseña"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+            </div>
+            {errorMessage && <p className="error">{errorMessage}</p>}
+            <button type="submit" className="login-button">Iniciar sesión</button>
+        </form>
+        <div className="register-link">
+            <p>No tienes cuenta? <a href="/register">Regístrate aquí</a></p>
         </div>
+    </div>
     );
 };
 
