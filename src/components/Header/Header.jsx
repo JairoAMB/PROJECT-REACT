71e1aa4d60c93de2132e9c3338433101f2a98a15
Jairo <jmasabanda@krugerschool.edu.ec>
2025-01-29 20:16:36 -0500

    Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,39 @@
-export const Header = () => {
+import React from 'react';
+import { Menubar } from 'primereact/menubar';
+import { Button } from 'primereact/button';
+import { Link } from 'react-router-dom';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
-    return(
-        <>
-            
-        </>
+const Header = () => {
+    const items = [
+        {
+            label: 'Home',
+            icon: 'pi pi-home',
+            command: () => window.location.href = '/'
+        },
+        {
+            label: 'Profile',
+            icon: 'pi pi-user',
+            command: () => window.location.href = '/profile'
+        },
+        {
+            label: 'My Flats',
+            icon: 'pi pi-building',
+            command: () => window.location.href = '/myflats'
+        }
+    ];
+
+    const end = (
+        <Button label="Logout" icon="pi pi-sign-out" className="p-button-outlined" onClick={() => window.location.href = '/login'} />
+    );
+
+    return (
+        <div>
+            <Menubar model={items} end={end} />
+        </div>
     );
 }
+
+export default Header;
